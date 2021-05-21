5f5b53a26e2484d849fb86b8648cc209f479a439
kulcua <17520834@gm.uit.edu.vn>
2021-05-21 13:27:58 +0700

    server ratchet

--- backend/src/Kulcua/Extension/Bundle/ChatBundle/WebSocket/Chat.php
+++ backend/src/Kulcua/Extension/Bundle/ChatBundle/WebSocket/Chat.php
@@ -1,18 +1,71 @@
 <?php
 namespace Kulcua\Extension\Bundle\ChatBundle\WebSocket;
+use Kulcua\Extension\Component\Message\Domain\Message;
+use OpenLoyalty\Bundle\UserBundle\Entity\User;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
 class Chat implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
     public function onOpen(ConnectionInterface $conn) {
+        // Store the new connection to send messages to later
+        $this->clients->attach($conn);
+
+        echo "New connection! ({$conn->resourceId})\n";
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
+        $numRecv = count($this->clients) - 1;
+        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+        $data = json_decode($msg, true);
+
+        // mess = new Message;
+        // $mess->setSenderId($data['userId']);
+        // $mess->setMessage($data['msg']);
+        // $mess->setLastMessageTimestamp(date("Y-m-d h:i:s"));
+        // if($objChatroom->saveChatRoom()) {
+        //     $objUser = new User;
+        //     $objUser->setId($data['userId']);
+        //     $user = $objUser->getUserById();
+        //     $data['from'] = $user['name'];
+        //     $data['msg']  = $data['msg'];
+        //     $data['dt']  = date("d-m-Y h:i:s");
+        // }
+
+        // foreach ($this->clients as $client) {
+        //     if ($from == $client) {
+        //         $data['from']  = "Me";
+        //     } else {
+        //         $data['from']  = $user['username'];
+        //     }
+        //     $client->send(json_encode($data));
+        // }
+
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                // The sender is not the receiver, send to each client connected
+                $client->send($msg);
+            }
+        }
     }
 
     public function onClose(ConnectionInterface $conn) {
+        // The connection is closed, remove it, as we can no longer send it messages
+        $this->clients->detach($conn);
+
+        echo "Connection {$conn->resourceId} has disconnected\n";
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "An error has occurred: {$e->getMessage()}\n";
+
+        $conn->close();
     }
 }
---
+++ frontend/src/modules/admin.chat/module.js
@@ -0,0 +1,60 @@
+import ChatController from "./ChatController";
+import ChatService from "./ChatService";
+
+const MODULE_NAME = "admin.chat";
+
+angular
+  .module(MODULE_NAME, [])
+  .config(($stateProvider) => {
+    $stateProvider
+      .state("admin.chat", {
+        url: "/chat",
+        views: {
+          "main@": {
+            templateUrl: require("./templates/chat.html"),
+            controller: "ChatController",
+            controllerAs: "ChatCtrl",
+          },
+        }
+      })
+      .state("admin.chat.chat-screen", {
+        url: "/chat-screen",
+        views: {
+          "mess": {
+            templateUrl: require("./templates/chat-screen.html"),
+            controller: "ChatController",
+            controllerAs: "ChatCtrl",
+          },
+        },
+        params: {
+          conversation: null,
+        },
+      });
+  })
+  .run(($templateCache, $http) => {
+    let catchErrorTemplate = () => {
+      throw `${MODULE_NAME} has missing template`;
+    };
+
+    $http
+      .get(`templates/chat.html`)
+      .then((response) => {
+        $templateCache.put("templates/chat.html", response.data);
+      })
+      .catch(catchErrorTemplate);
+
+    $http
+      .get(`templates/chat-screen.html`)
+      .then((response) => {
+        $templateCache.put("templates/chat-screen.html", response.data);
+      })
+      .catch(catchErrorTemplate);
+  })
+  .controller("ChatController", ChatController)
+  .service("ChatService", ChatService);
+
+try {
+  window.OpenLoyaltyConfig.modules.push(MODULE_NAME);
+} catch (err) {
+  throw `${MODULE_NAME} will not be registered`;
+}
