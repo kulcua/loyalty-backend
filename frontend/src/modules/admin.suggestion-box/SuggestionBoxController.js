2d960cbda1f358d7c08ab6a1184796bb4401b5b9
kulcua <17520834@gm.uit.edu.vn>
2021-06-29 16:01:44 +0700

    SuggestionDetail --no-img

--- frontend/src/modules/admin.suggestion-box/SuggestionBoxController.js
+++ frontend/src/modules/admin.suggestion-box/SuggestionBoxController.js
@@ -35,22 +35,6 @@ export default class SuggestionBoxController {
     this.config = DataService.getConfig();
     this.DataService = DataService;
     this.$scope.fileValidate = {};
-    // If 'required: true' it will only
-    // be required on default language
-    // this.$scope.translatableFields = [
-    //   {
-    //     key: "name",
-    //     label: "level.name",
-    //     prompt: "level.name_prompt",
-    //     required: true,
-    //   },
-    //   {
-    //     key: "description",
-    //     label: "level.description",
-    //     prompt: "level.description_prompt",
-    //     required: false,
-    //   },
-    // ];
     this.$scope.availableFrontendTranslations = this.DataService.getAvailableFrontendTranslations();
     this.active = [
       {
@@ -75,42 +59,6 @@ export default class SuggestionBoxController {
       userList: true,
       coverLoader: false,
     };
-    // this.testSuggestionBox = [
-    //   {
-    //     id : "1",
-    //     senderPhone: "090909009",
-    //     senderName: "Kim Phuong",
-    //     problemType: "Buon Ngu",
-    //     description: "Lam quai khong het deadline moi ng oi",
-    //     active: true,
-    //     photo: "asdfsaf",
-    //     timestamp : "12",
-    //     showModal: false
-    //   },
-    //   {
-    //     id:"2",
-    //     senderPhone: "090909009",
-    //     senderName: "Kim Phuong",
-    //     problemType: "Buon Ngu",
-    //     description: "Lam quai khong het deadline moi ng oi",
-    //     active: true,
-    //     photo: "asdfsaf",
-    //     timestamp : "12",
-    //     showModal: false
-    //   },
-    //   {
-    //     id:"3",
-    //     senderPhone: "090909009",
-    //     senderName: "Kim Phuong",
-    //     problemType: "Buon Ngu",
-    //     description: "Lam quai khong het deadline moi ng oi",
-    //     active: true,
-    //     photo: "asdfsaf",
-    //     timestamp : "12",
-    //     showModal: false
-    //   }
-    // ]
-     
   }
 
   getData() {
@@ -151,286 +99,66 @@ export default class SuggestionBoxController {
     );
   }
 
-  // getLevelCustomersData() {
-  //   let self = this;
+  _getSuggestionBox() {
+    let self = this;
+    self.loaderStates.suggestionBoxDetails = true;
+    if (self.suggestionBoxId) {
+      self.SuggestionBoxService.getSuggestionBox(self.suggestionBoxId)
+        .then(
+          res => {
+            self.$scope.suggestionBox = res;
+            self.$scope.editableFields = self.EditableMap.humanizeCampaign(res);
 
-  //   if (self.levelId) {
-  //     self.tableCustomerParams = new self.NgTableParams(
-  //       {},
-  //       {
-  //         getData: function (params) {
-  //           let dfd = self.$q.defer();
-  //           self.loaderStates.userList = true;
+            if (self.$scope.editableFields.levels && self.$scope.editableFields.levels.length) {
+              let levels = self.$scope.editableFields.levels;
+              for (let i in levels) {
+                let level = _.find(self.levels, { id: levels[i] });
+              }
 
-  //           self.SuggestionBoxService.getLevelCustomers(
-  //             self.ParamsMap.params(params.url()),
-  //             self.levelId
-  //           ).then(
-  //             function (res) {
-  //               self.$scope.customers = res;
-  //               params.total(res.total);
-  //               self.loaderStates.userList = false;
-  //               self.loaderStates.coverLoader = false;
-  //               dfd.resolve(res);
-  //             },
-  //             function () {
-  //               let suggestionBox = self.$filter("translate")(
-  //                 "xhr.get_level_customers.error"
-  //               );
-  //               self.Flash.create("danger", suggestionBox);
-  //               self.loaderStates.userList = false;
-  //               self.loaderStates.coverLoader = false;
-  //               dfd.reject();
-  //             }
-  //           );
+            }
+            if (self.$scope.editableFields.segments && self.$scope.editableFields.segments.length) {
+              let segments = self.$scope.editableFields.segments;
+              for (let i in segments) {
+                let segment = _.find(self.segments, { id: segments[i] });
+              }
 
-  //           return dfd.promise;
-  //         },
-  //       }
-  //     );
-  //   } else {
-  //     self.$state.go("admin.levels-list");
-  //     let suggestionBox = self.$filter("translate")("xhr.get_level_customers.no_id");
-  //     self.Flash.create("warning", suggestionBox);
-  //     self.loaderStates.userList = false;
-  //     self.loaderStates.coverLoader = false;
-  //   }
-  // }
+            }
+            if (self.$scope.editableFields.pos && self.$scope.editableFields.pos.length) {
+              let poses = self.$scope.editableFields.pos;
+              for (let i in poses) {
+                let pos = _.find(self.pos, { id: poses[i] });
+              }
 
-  // getLevelData() {
-  //   let self = this;
+            }
+            self.$scope.editableFields = self.EditableMap.humanizeSuggestionBoxFields(res);
+            self.loaderStates.suggestionBoxDetails = false;
+          },
+          () => {
+            let message = self.$filter('translate')('xhr.get_suggestion_box.error');
+            self.Flash.create('danger', message);
+            self.loaderStates.suggestionBoxDetails = false;
+          }
+        );
 
-  //   if (self.levelId) {
-  //     self.SuggestionBoxService.getLevel(self.levelId).then(
-  //       (res) => {
-  //         self.$scope.level = res;
-  //         self.$scope.editableFields = self.EditableMap.humanizeLevel(res);
-  //         self.loaderStates.coverLoader = false;
-  //       },
-  //       () => {
-  //         let suggestionBox = self.$filter("translate")("xhr.get_levels_list.error");
-  //         self.Flash.create("danger", suggestionBox);
-  //         self.loaderStates.coverLoader = false;
-  //       }
-  //     );
+      self.SuggestionBoxService.getSuggestionBoxImage(self.suggestionBoxId)
+        .then(
+          res => {
+            self.$scope.suggestionBoxImagePath = true;
+          }
+        )
+        .catch(
+          err => {
+            self.$scope.suggestionBoxImagePath = false;
+          }
+        );
 
-  //     self.SuggestionBoxService.getLevelImage(self.levelId)
-  //       .then((res) => {
-  //         self.$scope.levelImagePath = true;
-  //       })
-  //       .catch((err) => {
-  //         self.$scope.levelImagePath = false;
-  //       });
-  //   } else {
-  //     self.$state.go("admin.levels-list");
-  //     let suggestionBox = self.$filter("translate")("xhr.get_single_level.no_id");
-  //     self.Flash.create("warning", suggestionBox);
-  //     self.loaderStates.coverLoader = false;
-  //   }
-  // }
-
-  // getLevelCsvData(levelId, levelName) {
-  //   let self = this;
-  //   if (levelId) {
-  //     self.SuggestionBoxService.getFile(levelId).then(function (res) {
-  //       let date = new Date();
-  //       let filename =
-  //         levelName.replace(" ", "-") +
-  //         "-" +
-  //         date.toISOString().substring(0, 10) +
-  //         ".csv";
-  //       let blob = new Blob([res], { type: "text/csv" });
-  //       if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-  //         window.navigator.msSaveOrOpenBlob(blob);
-  //         return;
-  //       }
-
-  //       const data = window.URL.createObjectURL(blob);
-
-  //       let link = document.createElement("a");
-  //       link.href = data;
-  //       link.download = filename;
-  //       self.$timeout(function () {
-  //         link.dispatchEvent(new MouseEvent("click"));
-  //       }, 2000);
-  //     });
-  //   }
-  // }
-
-  // addLevel(newLevel) {
-  //   let self = this;
-
-  //   self.SuggestionBoxService.postLevel(newLevel).then(
-  //     (res) => {
-  //       if (self.$scope.levelImage) {
-  //         self.$scope.fileValidate = {};
-
-  //         self.SuggestionBoxService.postLevelImage(res.id, self.$scope.levelImage)
-  //           .then((res2) => {
-  //             self.$state.go("admin.levels-list", { levelId: self.levelId });
-  //             let suggestionBox = self.$filter("translate")(
-  //               "xhr.post_single_level.success"
-  //             );
-  //             self.Flash.create("success", suggestionBox);
-  //           })
-  //           .catch((err) => {
-  //             self.$scope.fileValidate = self.Validation.mapSymfonyValidation(
-  //               err.data
-  //             );
-  //             self.SuggestionBoxService.storedFileError = self.$scope.fileValidate;
-
-  //             let suggestionBox = self.$filter("translate")(
-  //               "xhr.post_single_level.warning"
-  //             );
-  //             self.Flash.create("warning", suggestionBox);
-
-  //             self.$state.go("admin.edit-level", { levelId: res.id });
-  //           });
-  //       } else {
-  //         self.$state.go("admin.levels-list");
-  //         let suggestionBox = self.$filter("translate")(
-  //           "xhr.post_single_level.success"
-  //         );
-  //         self.Flash.create("success", suggestionBox);
-  //       }
-  //     },
-  //     (res) => {
-  //       self.$scope.validate = self.Validation.mapSymfonyValidation(res.data);
-  //       let suggestionBox = self.$filter("translate")("xhr.post_single_level.error");
-  //       self.Flash.create("danger", suggestionBox);
-  //     }
-  //   );
-  // }
-
-  // editLevel(editedLevel) {
-  //   let self = this;
-
-  //   self.SuggestionBoxService.putLevel(editedLevel).then(
-  //     (res) => {
-  //       if (self.$scope.levelImage) {
-  //         self.$scope.fileValidate = {};
-
-  //         self.SuggestionBoxService.postLevelImage(self.levelId, self.$scope.levelImage)
-  //           .then((res2) => {
-  //             self.$state.go("admin.levels-list", { levelId: self.levelId });
-  //             let suggestionBox = self.$filter("translate")(
-  //               "xhr.put_single_level.success"
-  //             );
-  //             self.Flash.create("success", suggestionBox);
-  //           })
-  //           .catch((err) => {
-  //             self.$scope.fileValidate = self.Validation.mapSymfonyValidation(
-  //               err.data
-  //             );
-  //             self.SuggestionBoxService.storedFileError = self.$scope.fileValidate;
-
-  //             let suggestionBox = self.$filter("translate")(
-  //               "xhr.put_single_level.warning"
-  //             );
-  //             self.Flash.create("warning", suggestionBox);
-
-  //             self.$state.go("admin.edit-level", { levelId: self.levelId });
-  //           });
-  //       } else {
-  //         let suggestionBox = self.$filter("translate")(
-  //           "xhr.put_single_level.success"
-  //         );
-  //         self.Flash.create("success", suggestionBox);
-  //         self.$state.go("admin.levels-list");
-  //       }
-  //     },
-  //     (res) => {
-  //       self.$scope.validate = self.Validation.mapSymfonyValidation(res.data);
-  //       let suggestionBox = self.$filter("translate")("xhr.put_single_level.error");
-  //       self.Flash.create("danger", suggestionBox);
-  //     }
-  //   );
-  // }
-
-  // addSpecialReward(edit) {
-  //   let self = this;
-  //   let level;
-
-  //   if (!edit) {
-  //     level = self.$scope.newLevel;
-  //   } else {
-  //     level = self.$scope.editableFields;
-  //   }
-
-  //   level.specialRewards.push({
-  //     active: 0,
-  //     startAt: "",
-  //     endAt: "",
-  //   });
-  // }
-
-  // removeSpecialReward(index, edit) {
-  //   let self = this;
-  //   let level;
-
-  //   if (!edit) {
-  //     level = self.$scope.newLevel;
-  //   } else {
-  //     level = self.$scope.editableFields;
-  //   }
-
-  //   level.specialRewards = _.difference(level.specialRewards, [
-  //     level.specialRewards[index],
-  //   ]);
-  // }
-
-  // setLevelState(state, levelId) {
-  //   let self = this;
-
-  //   self.SuggestionBoxService.postActivateLevel(state, levelId).then(
-  //     () => {
-  //       let suggestionBox = self.$filter("translate")(
-  //         "xhr.post_activate_level.success"
-  //       );
-  //       self.Flash.create("success", suggestionBox);
-  //       self.tableParams.reload();
-  //     },
-  //     () => {
-  //       let suggestionBox = self.$filter("translate")(
-  //         "xhr.post_activate_level.error"
-  //       );
-  //       self.Flash.create("danger", suggestionBox);
-  //     }
-  //   );
-  // }
-
-  // /**
-  //  * Deletes photo
-  //  *
-  //  * @method deletePhoto
-  //  */
-  // deletePhoto() {
-  //   let self = this;
-
-  //   this.SuggestionBoxService.deleteLevelImage(this.levelId)
-  //     .then((res) => {
-  //       self.$scope.levelImagePath = false;
-  //       let suggestionBox = self.$filter("translate")(
-  //         "xhr.delete_level_image.success"
-  //       );
-  //       self.Flash.create("success", suggestionBox);
-  //     })
-  //     .catch((err) => {
-  //       self.$scope.validate = self.Validation.mapSymfonyValidation(res.data);
-  //       let suggestionBox = self.$filter("translate")("xhr.delete_level_image.error");
-  //       self.Flash.create("danger", suggestionBox);
-  //     });
-  // }
-
-  // /**
-  //  * Generating photo route
-  //  *
-  //  * @method generatePhotoRoute
-  //  * @returns {string}
-  //  */
-  // generatePhotoRoute() {
-  //   return this.config.apiUrl + "/level/" + this.levelId + "/photo";
-  // }
+    } else {
+      self.$state.go('admin.suggestion-box-list');
+      let message = self.$filter('translate')('xhr.get_suggestion_box.no_id');
+      self.Flash.create('warning', message);
+      self.loaderStates.suggestionBoxDetails = false;
+    }
+  }
 }
 
 SuggestionBoxController.$inject = [
--- frontend/src/modules/admin.suggestion-box/SuggestionBoxService.js
+++ frontend/src/modules/admin.suggestion-box/SuggestionBoxService.js
@@ -5,37 +5,28 @@ export default class SuggestionBoxService {
     }
 
     getSuggestionBoxs(params) {
-        return this.Restangular.all('suggestion_box').one('suggestion_box').getList(params);
+        return this.Restangular.all('suggestion_box').getList(params);
     }
-
-    // getLevel(levelId) {
-    //     return this.Restangular.one('level', levelId).get();
-    // }
-
-    // postLevel(newLevel) {
-    //     return this.Restangular.one('level').one('create').customPOST({level: newLevel})
-    // }
-
-    // getLevelCustomers(params, levelId) {
-    //     if(!params) {
-    //         params = {}
-    //     }
-    //     return this.Restangular.one('level', levelId).all('customers').getList();
-    // }
-    // getFile(levelId) {
-    //     return this.Restangular.one('csv').one('level', levelId).get();
-    // }
-    // putLevel(editedLevel) {
-    //     let self = this;
-
-    //     return editedLevel.customPUT({level: self.EditableMap.level(editedLevel)});
-    // }
 
     // postActivateLevel(state, levelId) {
     //     let self = this;
 
     //     return this.Restangular.one('level').one(levelId).one('activate').customPOST({active: state})
     // }
+
+    /**
+     * Calls for suggestionbox images
+     *
+     * @method getSuggestionBoxImages
+     * @param {Integer} suggestionboxId
+     * @returns {Promise}
+     */
+     getSuggestionBoxImage(suggestionboxId) {
+        return this.Restangular
+            .one('suggestion_box')
+            .one('photo', suggestionboxId)
+            .get()
+    }
 }
 
 SuggestionBoxService.$inject = ['Restangular', 'EditableMap'];
